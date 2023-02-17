1ba7f76f88f1446f63d3f8019f986b4cfc002e37
Virginia Mwenda <mwendavirgy@gmail.com>
2023-02-17 15:45:55 -0800

    Updated task #2 to betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,10 @@
 #include <unistd.h>
 
 /**
-Advanced task #2
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
